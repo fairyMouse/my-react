428b43b85f93a75e5fb6331c50643fbdabe88178
fairyMouse <1065915112@qq.com>
2023-09-12 12:08:11 +0800

    feat: jsx createElement

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,66 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import type {
+	ElementType,
+	Key,
+	Props,
+	ReactElementType,
+	Ref
+} from 'shared/ReactTypes';
+
+const ReactElement = function (
+	type: ElementType,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElementType {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'feihan' // 和真实的react项目区分一下
+	};
+	return element;
+};
+
+export function jsx(type: ElementType, config: any, ...maybeChildren: any) {
+	// NOTE: 此处的 jsx 方法统一了 React.createElement 的处理
+	// 实际情况下，两者的实现不相同
+
+	const props: Props = {};
+
+	let key: Key = null;
+	let ref: Ref = null;
+	for (const propName in config) {
+		// 筛选出 key 和 ref，其他的都作为 props
+		const val = config[propName];
+		if (propName === 'key') {
+			if (val !== undefined) {
+				key = `${val}`;
+			}
+		} else if (propName === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+		} else if ({}.hasOwnProperty.call(config, propName)) {
+			// 排除原型链上的属性
+			props[propName] = val;
+		}
+	}
+
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+	return ReactElement(type, key, ref, props);
+}
+
+export function jsxDEV(type: ElementType, config: any) {
+	// 真实的react会在这里做一些额外的检查
+	return jsx(type, config);
+}
---
+++ packages/shared/ReactTypes.ts
@@ -0,0 +1,25 @@
+export type ElementType = any;
+export type Props = any;
+export type Key = any;
+export type Ref = any;
+
+export interface ReactElementType {
+	/**
+	 * 内部字段，用于标识当前对象是一个 ReactElement。
+	 * 需要通过 Symbol 来保证唯一性
+	 */
+	$$typeof: symbol | number;
+	type: ElementType;
+	props: Props;
+	key: Key;
+	ref: Ref;
+	/**
+	 * 自定义字段，真实 react 项目中不存在
+	 */
+	__mark: string;
+}
+
+/**
+ * 支持传递一个值或者函数，函数的返回值作为新的 state
+ */
+export type Action<State> = State | ((prevState: State) => State);
